0eaf6b7c78e4776aa825f97cf13c6c65823dc5b1
DuongAlern <142880959+Duongtunggiang@users.noreply.github.com>
2024-07-26 15:51:06 +0700

    Add files via upload

--- car/src/main/java/com/group/car/config/SecurityConfig.java
+++ car/src/main/java/com/group/car/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.group.car.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,25 +8,35 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/").permitAll()
-                .requestMatchers("/store/**").permitAll()
-                .requestMatchers("/register").permitAll()
-                .requestMatchers("/login").permitAll()
-                .requestMatchers("/logout").permitAll()
-                .anyRequest().authenticated())
-                .formLogin(form -> form.defaultSuccessUrl("/", true))
-                .logout(congig -> congig.logoutSuccessUrl("/")).build();
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/store/**").permitAll()
+                        .requestMatchers("/register").permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/logout").permitAll()
+                        .requestMatchers("/home").hasRole("CUSTOMER")
+                        .requestMatchers("/home-driver").hasRole("DRIVER")
+//                        .requestMatchers("/dashboard").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .successHandler(customAuthenticationSuccessHandler)
+                        .permitAll())
+                .logout(config -> config.logoutSuccessUrl("/").permitAll())
+                .build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return  new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 }
